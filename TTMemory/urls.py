488c6faf80f796a7612fde2253a79918c36bb12f
xurunkang <tsuiyuenhong@gmail.com>
2022-01-18 21:14:16 +0800

    add upload performance log

--- TTMemory/urls.py
+++ TTMemory/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('show_memory', views.show_memory, name='TTMemory'),
     path('fetch_memory', views.fetch_memory, name='TTMemory'),
+    path('index', views.index, name='TTMemory'),
+    path('upload_performance_json', views.upload_performance_json, name='TTMemory'),
 ]
+
--- TTMemory/views.py
+++ TTMemory/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 import json
 from random import randrange
 
 from .performance_helper import PerformanceHelper as PH
+from django.views.decorators.csrf import csrf_exempt
+
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+from django.shortcuts import redirect
 
 def response_as_json(data):
     json_str = json.dumps(data)
@@ -24,7 +29,17 @@ def json_response(data, code=200):
 JsonResponse = json_response
 
 def fetch_memory(request):
-    return JsonResponse(json.loads(PH.map_memory_json_to_line('./jsons/raw_memory_records.json')))
+    return JsonResponse(json.loads(PH.map_memory_json_to_line('./jsons/performance.json')))
 
 def show_memory(request):
     return HttpResponse(content=open('./templates/index.html').read())
+
+def index(request):
+    return HttpResponse(content=open('./templates/upload.html').read())
+
+@csrf_exempt
+def upload_performance_json(request):
+    performance_json = request.FILES.get('performance_json')
+    default_storage.delete('./jsons/performance.json')
+    default_storage.save('./jsons/performance.json', ContentFile(performance_json.read()))
+    return HttpResponse(JsonResponse({}))
